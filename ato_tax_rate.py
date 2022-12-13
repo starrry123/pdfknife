2b55548c9c7ceaa393d3c91b75581ed6b0706f15
starrry123 <59469463+starrry123@users.noreply.github.com>
2022-12-13 13:38:08 +0800

    Update ato_tax_rate.py

--- ato_tax_rate.py
+++ ato_tax_rate.py
@@ -16,16 +16,21 @@ def cal_tax(m):
 income=0
 tax_data=[]
 income_data=[]
-k=200
-for i in range(0,k):
+real_tax_rate=[]
+
+for i in range(0,300): #income 0-300K
     income+=1000
     income_data.append(income)
+
     tax_data.append(cal_tax(income))
+    real_tax_rate.append(cal_tax(income)/income)    
     tax_rate=[[180000,0.45,51667],
           [120000,0.37,29467],
           [45000,0.325,5092],
           [18200,0.19,0]]
 
 import plotly.express as px
-fig = px.line(x=income_data,y=tax_data,title='ATO tax rate')
+fig = px.line(x=income_data,y=real_tax_rate,title='ATO Actual Tax Rate VS Your Income')
+fig.layout.yaxis.tickformat = ',.0%'
 fig.show()
+
